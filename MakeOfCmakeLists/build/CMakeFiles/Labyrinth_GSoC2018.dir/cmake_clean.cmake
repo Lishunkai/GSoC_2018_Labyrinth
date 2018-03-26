67d43ab47f931fbfbefaef9068a142ca96fae8ba
Lishunkai <worldprc@qq.com>
2018-03-26 20:42:37 +0800

    update

---
+++ MakeOfCmakeLists/build/CMakeFiles/Labyrinth_GSoC2018.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labyrinth_GSoC2018.dir/main.cpp.o"
+  "CMakeFiles/Labyrinth_GSoC2018.dir/Labyrinth.cpp.o"
+  "Labyrinth_GSoC2018.pdb"
+  "Labyrinth_GSoC2018"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Labyrinth_GSoC2018.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
